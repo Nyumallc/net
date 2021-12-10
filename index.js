22cad0847bcc3291c69cfae9a845c56e15b2d016
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2021-12-10 14:18:52 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+btn = document.getElementById("submit-btn");
+btn.setAttribute('onclick', 'pushButton1()');
+function pushButton1() {
+    let name = document.getElementById("user-name");
+    let displayname = name.innerHTML;
+    let user_id = document.getElementById("user_id");
+    let userid = user_id.innerHTML;
+    let url = document.getElementById("url");
+    let urltext = url.value;
+    let option= document.getElementById("option");
+    let optiontext= option.value;
+    
+    let resurl=(`${REQUEST_URL}?&userid=${userid}&displayname=${displayname}&url=${urltext}&option=${optiontext}`)
+
+         alert(resurl);
+        const res = fetch(resurl);
+        console.log(res);
+    };
